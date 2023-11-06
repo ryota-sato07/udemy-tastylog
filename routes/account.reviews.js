0ec53a7ef49660287024dec7929910bf631bed04
ryota-sato07 <satour.0722@gmail.com>
2023-11-06 09:05:18 +0900

    regist confirm back page

--- routes/account.reviews.js
+++ routes/account.reviews.js
@@ -31,6 +31,12 @@ router.get("/regist/:shopId(\\d+)", async (req, res, next) => {
   }
 });
 
+router.post("/regist/:shopId(\\d+)", async (req, res, next) => {
+  var review = createReviewData(req);
+  var { shopId, shopName } = req.body;
+  res.render("./account/reviews/regist-form.ejs", { shopId, shopName, review });
+});
+
 router.post("/regist/confirm", (req, res, next) => {
   var review = createReviewData(req);
   var { shopId, shopName } = req.body;
